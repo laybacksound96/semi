cc3e31778ba56ebbf86ec6b591b8391a80b56434
412 <laybacksound96@gmail.com>
2023-05-26 17:49:22 +0900

    init

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import PageContent from "../components/PageContent";
+
+function HomePage() {
+  return (
+    <PageContent title="Welcome!">
+      <p>분노에 가득 찬 강아지들을 만나보세요!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
